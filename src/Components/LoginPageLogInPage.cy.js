200fee7874a1cfdc6e79e1699f2991e8b7c400b6
Ichcha Baliga <42496215+ichchabaliga@users.noreply.github.com>
2023-12-09 22:47:28 -0500

    added unit testand E2E test

--- src/Components/LoginPageLogInPage.cy.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import LogInPage from './LoginPage'
-
-describe('<LogInPage />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<LogInPage />)
-  })
-})
--- src/Components/SignUpFormSignupForm.cy.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import SignupForm from './SignUpForm'
-
-describe('<SignupForm />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<SignupForm />)
-  })
-})
